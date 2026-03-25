66b0985a539b64ce7b211de7d6657e413987218d
Komal Verma <komalverma22012005@gmail.com>
2026-03-25 09:31:28 -0700

    added: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
+           <Analytics />
       </body>
     </html>
   );
